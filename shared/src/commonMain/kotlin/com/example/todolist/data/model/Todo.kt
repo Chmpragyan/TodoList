7ed8383eaeee136d9658c08ef1bd3fce9ead2ea9
admin <admin@admins-Mac-mini.local>
2026-05-11 08:55:42 +0545

    feat: DIAL-3942- add data model to shared module.

--- shared/src/commonMain/kotlin/com/example/todolist/data/model/Todo.kt
+++ shared/src/commonMain/kotlin/com/example/todolist/data/model/Todo.kt
@@ -1,2 +1,13 @@
-package com.example.todolist.data.model 
+package com.example.todolist.data.model
 
+import kotlin.time.Clock
+import kotlin.uuid.ExperimentalUuidApi
+import kotlin.uuid.Uuid
+
+@OptIn(ExperimentalUuidApi::class)
+data class Todo(
+    val id: String = Uuid.random().toString(),
+    val title: String,
+    val description: String,
+    val createdAt: Long = Clock.System.now().toEpochMilliseconds()
+)
