17836c6c1029eff78906d3f9acf504a87db75f7b
Pragyan <pragyancham07@gmail.com>
2026-05-12 07:43:05 +0545

    wip: DIAL-3950 - update repository to map database results to todo model.

--- shared/src/commonMain/kotlin/com/example/todolist/data/repository/TodoRepository.kt
+++ shared/src/commonMain/kotlin/com/example/todolist/data/repository/TodoRepository.kt
@@ -1,20 +1,40 @@
 package com.example.todolist.data.repository
 
-import com.example.todolist.Todo
+import com.example.todolist.data.model.Todo
 import com.example.todolist.database.Database
 
 class TodoRepository(database: Database) {
-
     private val queries = database.todoQueries
 
-    fun getAllTodos(): List<Todo> = queries.selectAllTodo().executeAsList()
-
-    fun addTodo(title: String, description: String) {
-        queries.insertTodo(title, description)
+    fun getTodos(): List<Todo> {
+        return queries
+            .selectAllTodo()
+            .executeAsList()
+            .map {
+                Todo(
+                    title = it.title,
+                    description = it.description
+                )
+            }
     }
 
-    fun updateTodo(id: Long, title: String, description: String) {
-        queries.updateTodo(title, description, id)
+    fun addTodo(title: String, description: String) {
+        queries.insertTodo(
+            title = title,
+            description = description,
+        )
+    }
+
+    fun updateTodo(
+        id: Long,
+        title: String,
+        description: String
+    ) {
+        queries.updateTodo(
+            id = id,
+            title = title,
+            description = description
+        )
     }
 
     fun deleteTodo(id: Long) {
