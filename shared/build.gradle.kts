d04633ec0ce0f09357a55cbfd0259f174a8e1f5b
Pragyan <pragyancham07@gmail.com>
2026-05-14 11:38:39 +0545

    wip: DIAL-3978 - update iOS project configuration and link SQLite and SwiftUI frameworks.
    - Update `IPHONEOS_DEPLOYMENT_TARGET` to 16.0 and `SWIFT_VERSION` to 6.0 in the Xcode project.
    - Add `-lsqlite3` and `SwiftUI` framework to linker options in `shared/build.gradle.kts` and `Config.xcconfig`.
    - Refactor and update build settings for Debug and Release configurations in `project.pbxproj`, including debug information formats and compilation modes.
    - Add various build flags to `Config.xcconfig` to manage ABI checking, asset symbols, and linker behavior.

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -20,6 +20,7 @@ kotlin {
         iosTarget.binaries.framework {
             baseName = "Shared"
             isStatic = true
+            linkerOpts("-lsqlite3", "-framework", "SwiftUI")
         }
     }
 
