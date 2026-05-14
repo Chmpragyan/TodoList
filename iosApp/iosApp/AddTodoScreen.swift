bfac5269a0c10834f67dfcada01984cfc2793e70
Pragyan <pragyancham07@gmail.com>
2026-05-14 13:41:20 +0545

    wip: DIAL-3978 - Implement AddTodoScreen UI and update ContentView.

--- iosApp/iosApp/AddTodoScreen.swift
+++ iosApp/iosApp/AddTodoScreen.swift
@@ -6,7 +6,41 @@ import Foundation
 import SwiftUI
 
 struct AddTodoScreen: View {
-    var body: some View {
+    @Environment(\.dismiss) private var dismiss
 
+    @State private var title = ""
+    @State private var description = ""
+
+    var body: some View {
+        Form {
+            TextField("Todo", text: $title)
+                .padding()
+                .cornerRadius(10)
+                .textFieldStyle(.plain)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 10)
+                        .stroke(Color.gray, lineWidth: 1)
+                )
+
+            TextField("Description", text: $description)
+                .padding()
+                .cornerRadius(10)
+                .textFieldStyle(.plain)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 10)
+                        .stroke(Color.gray, lineWidth: 1)
+                )
+        }
+        .textFieldStyle(.roundedBorder)
+
+        Button(action: {}) {
+            Text("Add Todo")
+                .padding()
+                .frame(maxWidth: .infinity)
+                .background(Color.gray)
+                .foregroundColor(.white)
+                .cornerRadius(10)
+        }
+        .padding(.horizontal)
     }
 }
