33508b257aea6d1fa739e95b8b8316f9eee965ab
Pragyan <pragyancham07@gmail.com>
2026-05-15 08:20:12 +0545

    wip: DIAL-4020 - implement IOSTodoViewModel for iOS application.
    - Create `IOSTodoViewModel` as an `ObservableObject` to manage state in the iOS app.
    - Expose `TodoState` via a `@Published` property for SwiftUI integration.
    - Implement methods for CRUD operations (load, add, update, delete) and todo selection.

---
+++ iosApp/iosApp/IOSTodoViewModel.swift
@@ -0,0 +1,41 @@
+//
+// Created by admin on 15/05/2026.
+//
+
+import Foundation
+import SwiftUI
+import Shared
+
+@MainActor
+class IOSTodoViewModel: ObservableObject {
+    @Published var state = TodoState(todos: [], editTodo: nil)
+    private let viewModel = IOSTodoViewModel()
+
+    init() {
+
+    }
+
+    func loadTodos() {
+        viewModel.loadTodos()
+    }
+
+    func addTodo(title: String, description: String) {
+        viewModel.addTodo(title: title, description: description)
+    }
+
+    func updateTodo(id: Int64, title: String, description: String) {
+        viewModel.updateTodo(id: id, title: title, description: description)
+    }
+
+    func selectTodo(todo: TodoModel) {
+        viewModel.selectTodo(todo: todo)
+    }
+
+    func deleteTodo(id: Int64) {
+        viewModel.deleteTodo(id: id)
+    }
+
+    // deinit {
+    //     viewModel.clear()
+    // }
+}
